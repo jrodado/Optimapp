06f49548ac0288e0afad112898e2fe1d9d4a386d
jrodado <128794330+jrodado@users.noreply.github.com>
2023-10-29 18:34:20 +0100

    Login con gmail. Inicio

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,11 @@ dependencies {
 
     // Add the dependencies for any other desired Firebase products
     // https://firebase.google.com/docs/android/setup#available-libraries
+
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth")
 }
---
+++ app/src/main/java/com/silvia/optimapp/AuthActivity.java
@@ -0,0 +1,19 @@
+package com.silvia.optimapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+
+        //Setup
+        //setup();
+    }
+
+
+}
